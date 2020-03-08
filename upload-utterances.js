d176407f014b53e454d1646e12693b1357438959
Zainab <ennybello@yahoo.co.uk>
2020-03-08 12:06:50 -0400

    added code to rebuild bot

--- upload-utterances.js
+++ upload-utterances.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 "use strict";
 
 const args = require("yargs").array("intents").argv;
@@ -9,19 +11,14 @@ const fs = require("fs");
 console.log("Spreadsheet: " + args.sheet);
 console.log("Intent List: " + args.intents);
 
-var aws_response1 = "";
-var aws_response2 = "";
-
 async function addUtterances() {
   args.intents.forEach(async intent => {
-
     // Read CSV File
     let utterances = await readCSV(args.sheet, intent);
-    console.log("Missed Data: ");
 
-    var getIntentCmd = `aws lex-models get-intent --region ${region_id} --name ${intent} --intent-version "\$LATEST" > intent.json`;
+    var getIntentCmd = `aws lex-models get-intent --region ${region_id} --name ${intent} --intent-version $LATEST > intent.json`;
     try {
-      aws_response1 = exec(getIntentCmd);
+      const aws_response1 = exec(getIntentCmd);
 
       // Read intent.json file
       fs.readFile("intent.json", function(err, data) {
@@ -36,22 +33,54 @@ async function addUtterances() {
           intentJson["sampleUtterances"].push(utterance);
         }
 
+        intentJson["sampleUtterances"] = removeDuplicates(intentJson["sampleUtterances"]);
+        console.log("iNTENT Json: ", intentJson);
+
         // Writing to a file
         fs.writeFile("intent.json", JSON.stringify(intentJson), err => {
           // Checking for errors
           if (err) throw err;
 
-          console.log("Done writing"); // Success
+          console.log("Done writing to intent.json"); // Success
           var updateIntendCmd = `aws lex-models put-intent --region ${region_id} --name ${intent} --cli-input-json file://intent.json`;
-          aws_response2 = exec(updateIntendCmd);
-          console.log("AWS Response: ", aws_response2);
+          const aws_response2 = exec(updateIntendCmd);
+          console.log("AWS Update Intent Response: ", aws_response2);
+          if (args.bot) {
+            console.log(`Rebuilding ${args.bot} Bot...`);
+            var getBotCmd = `aws lex-models get-bot --region ${region_id} --name ${args.bot} --version-or-alias $LATEST > bot.json`;
+            const getBotResponse = exec(getBotCmd);
+            console.log("Get Bot Response: ", getBotResponse);
+
+            // Read json file
+            fs.readFile("bot.json", function(err, data) {
+              console.log("Reading bot.json file");
+              // Check for errors
+              if (err) throw err;
+              // Converting to JSON
+              const botJson = JSON.parse(data);
+              delete botJson["createdDate"];
+              delete botJson["lastUpdatedDate"];
+              delete botJson["status"];
+              delete botJson["version"];
+              botJson["processBehavior"] = "BUILD";
+              // Writing to a file
+              fs.writeFile("bot.json", JSON.stringify(botJson), err => {
+                // Checking for errors
+                if (err) throw err;
+                console.log("Done writing to bot.json"); // Success
+                var rebuildBotCmd = `aws lex-models put-bot --region ${region_id} --name ${args.bot} --cli-input-json file://bot.json`;
+                const rebuildBotResponse = exec(rebuildBotCmd);
+                console.log("Rebuild Bot Response: ", rebuildBotResponse);
+              });
+            });
+          }
         });
       });
     } catch (error) {
       console.error(error);
       return;
     }
-    });
+  });
 }
 
 async function readCSV(sheet, intent) {
@@ -80,6 +109,16 @@ async function readCSV(sheet, intent) {
       reject(err);
     }
   });
+}
+
+function removeDuplicates(array) {
+  let a = [];
+  array.map(x => {
+    if (!a.includes(x)) {
+      a.push(x);
+    }
+  });
+  return a;
 }
 
 addUtterances();
